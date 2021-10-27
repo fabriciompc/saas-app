49c87cf1df0ed3ecdd507bdc90117d77b7b41919
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2021-10-27 14:55:46 -0300

    feature: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('SAAS App')
+    .setDescription('A multi purpose software as a service for study')
+    .setVersion('1.0')
+    .addTag('saas')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
